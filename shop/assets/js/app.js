61f7201413822c0c36fcb19df93ddf365e1ac2e3
Daria <darjana29@gmail.com>
2025-02-18 02:05:02 +0300

    feat: add new second submenu

--- shop/assets/js/app.js
+++ shop/assets/js/app.js
@@ -1,3 +1,21 @@
 // Display count-currency dropdown
 
-const countCurrency = document.querySelector(".count-currency");
+// Open/close sub-menu
+document.addEventListener('click', documentActions);
+
+const documentActions = (e) => {
+  const targetElement = e.target;
+  if (targetElement.closest('[data-parent]')) {
+    const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
+    const subMenu = document.querySelector(`[data-submenu = '${subMenuId}']`);
+
+    if (subMenu) {
+      targetElement.classList.toggle('_sub-menu-active');
+			subMenu.classList.toggle('_sub-menu-open');
+    } else {
+      console.log('Такого подменю нет!')
+    }
+
+    e.preventDefault();
+  }
+}
