6222ab709e2b78aa4fbbfa7c2a1ed42a66b80349
Daria <darjana29@gmail.com>
2025-04-02 23:35:29 +0300

    feat: add dinamic for filters

--- shop/assets/js/app.js
+++ shop/assets/js/app.js
@@ -1,19 +1,44 @@
+// Open/close filters list
+
+const catalogFiltersBtn = document.querySelector('.catalog-control__btn');
+
+catalogFiltersBtn.addEventListener('click', catalogBtnOpen);
+
+function catalogBtnOpen() {
+  const catalogControlBtnText = document.querySelector('.catalog-control__btn-text');
+  const catalogBody = document.querySelector('.catalog__body');
+  const filterCatalogItems = document.querySelector('.filter-catalog__items');
+  const productsCard = document.querySelector('.products__card');
+  
+  if (filterCatalogItems.classList.toggle('open-filter')) {
+    catalogBody.classList.add('open-filter');
+    productsCard.classList.add('open-filter');
+    catalogControlBtnText.textContent = 'Спрятать фильтры';
+  } else {
+    catalogBody.classList.remove('open-filter');
+    productsCard.classList.remove('open-filter');
+    catalogControlBtnText.textContent = 'Показать фильтры';
+  }
+};
+
 // Accardion
 
-const items = Array.from(document.querySelectorAll('.filter-catalog__item'));
 
-items.forEach(item => {
+
+
+/* catalogFilters.forEach(item => {
   item.addEventListener('click', itemHandler);
 });
 
 function itemHandler(e) {
-  e.preventDefault();
-  let currentItem = e.target.closest('.filter-catalog__item');
-  let currentContent = e.target.nextElementSibling;
-  currentItem.classList.toggle('open');
-  if (currentItem.classList.contains('open')) {
-    currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
+  //e.preventDefault();
+  let catalogFilters = e.target.closest('.filter-catalog__item');
+  let contentItem = e.target.nextElementSibling;
+  e.currentTarget.classList.toggle('open');
+  if (catalogFilters.classList.contains('open')) {
+    contentItem.style.maxHeight = contentItem.scrollHeight + 'px';
   } else {
-    currentContent.style.maxHeight = 0;
+    contentItem.style.maxHeight = 0;
   }
-}
+}  */
+
